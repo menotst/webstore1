7ab3a701f664d41a477025ba75cd69c0101b4296
menoik <as@as.ru>
2018-01-12 01:44:17 +0300

    сделать дз

--- webstore2/webstore2/Startup.cs
+++ webstore2/webstore2/Startup.cs
@@ -5,16 +5,24 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace webstore2
 {
     public class Startup
     {
+        public IConfiguration Configuration;
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+            
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,10 +32,18 @@ namespace webstore2
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseStaticFiles();
+            var hello = Configuration["CustomHelloWorld"];
 
-            app.Run(async (context) =>
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync(hello);
+            //});
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute
+                (name: "default",
+                template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
